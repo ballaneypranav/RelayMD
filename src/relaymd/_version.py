9371054b3287f3f81bad3c8c19455c2ee9c3459e
Pranav Ballaney <ballaneypranav@gmail.com>
2026-07-18 13:58:50 -0400

    Bump relaymd version to 0.2.19

--- src/relaymd/_version.py
+++ src/relaymd/_version.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
 
-FALLBACK_VERSION = "0.2.18"
+FALLBACK_VERSION = "0.2.19"
 
 
 def get_version() -> str:
