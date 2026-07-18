026d8d92e6102b4bc6f7610922081f055532254c
Pranav Ballaney <ballaneypranav@gmail.com>
2026-07-18 10:59:29 -0400

    Fix worker image smoke test

--- src/relaymd/_version.py
+++ src/relaymd/_version.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
 
-FALLBACK_VERSION = "0.2.13"
+FALLBACK_VERSION = "0.2.14"
 
 
 def get_version() -> str:
